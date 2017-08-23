750c94465f2094f5d3a766365777294d9b0871e3
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-08-23 00:16:21 -0300

    adding repository specs

---
+++ spec/controllers/repositories_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe RepositoriesController, type: :controller do
+  let(:valid_attributes) { {} }
+
+  describe "GET #index" do
+    it "returns a success response" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a success response" do
+      repository = Repository.create! valid_attributes
+      get :show, params: { id: repository.to_param }
+      expect(response).to be_success
+    end
+  end
+
+end
